46430fc2e2bb6126bb159bc5ae19c31467c0eccf
Marc Leibold <leibold485@gmail.com>
2021-01-03 20:36:18 +0100

    added example config and basic methods

--- espwebsite/config.py
+++ espwebsite/config.py
@@ -1,1 +1,35 @@
-config = {}
+config = {
+    "activeControllers": [
+        {
+            "name": "TestESP",
+            "customColors": [
+                "red",
+                "green",
+                "blue"
+            ]
+        },
+        {
+            "name": "TestESP2",
+            "customColors": [
+                "blue",
+                "green",
+                "blue"
+            ]
+        }
+    ]
+}
+
+
+def get(configEntry):
+    if configEntry in config:
+        return config[configEntry]
+    else:
+        return False
+
+
+def set(configEntry, value):
+    if configEntry in config:
+        config[configEntry] = value
+        return True
+    else:
+        return False
