3fcf9ef906629f0cc1e054898b275e8bd786b6f3
Marc Leibold <leibold485@gmail.com>
2021-01-05 17:00:12 +0100

    added proper navigation

--- espwebsite/routes.py
+++ espwebsite/routes.py
@@ -5,3 +5,18 @@ from espwebsite import app, config, helpers
 @ app.route('/', methods=['GET'])
 def base():
     return render_template("base.html", controllers=helpers.getControllersHTML(config.get('activeControllers')))
+
+
+@ app.route('/controls', methods=['GET'])
+def controls():
+    return helpers.getControllersHTML(config.get('activeControllers'))
+
+
+@ app.route('/setup', methods=['GET'])
+def setup():
+    return open("espwebsite/templates/setup.html").read()
+
+
+@ app.route('/network', methods=['GET'])
+def network():
+    return open("espwebsite/templates/network.html").read()
