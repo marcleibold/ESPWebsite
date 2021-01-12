f5f0c98b5870a920edf1ac7324df49ca1adc8590
Marc Leibold <leibold485@gmail.com>
2021-01-12 14:23:08 +0100

    implemented search and __init__

--- espwebsite/NetworkHandler.py
+++ espwebsite/NetworkHandler.py
@@ -1,14 +1,26 @@
+from wifi import Cell
+import netifaces
 
 
 class NetworkHandler:
 
+    wifiInterface = ""
+    ethInterface = ""
+
     def __init__(self):
-        pass
+        interfaces = netifaces.interfaces()
+        self.wifiInterface = [
+            interface for interface in interfaces if interface.startswith("wl") and not interface.startswith("wlx")
+        ][0]
+        self.ethInterface = [
+            interface for interface in interfaces if interface.startswith("e")
+        ][0]
 
     def search(self):
         """Get open Wifi networks nearby
         """
-        pass
+        wifiNetworks = Cell.all(self.wifiInterface)
+        return wifiNetworks
 
     def connect(self, client):
         """Connect a client to the main network
@@ -16,6 +28,7 @@ class NetworkHandler:
         Args:
             client (dict): client data
         """
+        # TODO: implement api call to connect ESP to wifi
         pass
 
     def queryNetworks(self, networks):
@@ -24,9 +37,13 @@ class NetworkHandler:
         Args:
             networks (list): list of networks to query
         """
+        # TODO: return dict of networks with containing devices
         pass
 
     def getMicrocontrollerNetworks(self):
         """get nearby networks which are hosted by ESP8266's
         """
+        available = self.search()
+        networkData = self.queryNetworks(available)
+        # TODO: get right networks out of networkData (dict)
         pass
