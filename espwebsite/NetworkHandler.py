308558cd27256ef59b05eb52815b8fd6826c83e3
Marc Leibold <leibold485@gmail.com>
2021-01-12 13:42:35 +0100

    base layout for NetworkHandler

---
+++ espwebsite/NetworkHandler.py
@@ -0,0 +1,32 @@
+
+
+class NetworkHandler:
+
+    def __init__(self):
+        pass
+
+    def search(self):
+        """Get open Wifi networks nearby
+        """
+        pass
+
+    def connect(self, client):
+        """Connect a client to the main network
+
+        Args:
+            client (dict): client data
+        """
+        pass
+
+    def queryNetworks(self, networks):
+        """Query Wifi Networks for Microcontrollers
+
+        Args:
+            networks (list): list of networks to query
+        """
+        pass
+
+    def getMicrocontrollerNetworks(self):
+        """get nearby networks which are hosted by ESP8266's
+        """
+        pass
