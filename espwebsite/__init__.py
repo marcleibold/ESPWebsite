346fae4a28fcb44130f5167f996f4c089a9255b8
Marc Leibold <leibold485@gmail.com>
2021-01-22 18:29:36 +0100

    added config file

--- espwebsite/__init__.py
+++ espwebsite/__init__.py
@@ -1,3 +1,12 @@
 from flask import Flask
+import os
+import json
+from espwebsite import config
+
+if "config.json" not in os.listdir():
+    with open("config.json", "a") as f:
+        json.dump(config.getDefault(), f)
+
+config.load()
 
 app = Flask(__name__)
--- espwebsite/config.py
+++ espwebsite/config.py
@@ -1,30 +1,13 @@
-config = {
-    "activeControllers": [
-        {
-            "name": "TestESP",
-            "customColors": [
-                "red",
-                "green",
-                "blue"
-            ]
-        },
-        {
-            "name": "TestESP2",
-            "customColors": [
-                "blue",
-                "green",
-                "blue"
-            ]
-        }
-    ],
-    "deviceData": {},
-    "connectedDevices": []
-}
+import json
+
+config = {}
 
 
 def get(configEntry):
     if configEntry in config:
+        save()
         return config[configEntry]
+
     else:
         return False
 
@@ -32,6 +15,7 @@ def get(configEntry):
 def set(configEntry, value):
     if configEntry in config:
         config[configEntry] = value
+        save()
         return True
     else:
         return False
@@ -41,19 +25,43 @@ def add(configEntry, value):
     if configEntry in config:
         if type(config[configEntry]) == list:
             config[configEntry].append(value)
+            save()
 
 
 def addController(controller):
     config["activeControllers"].append(controller)
+    save()
 
 
 def removeController(name):
     for i, controller in enumerate(config["activeControllers"]):
         if controller["name"] == name:
             config["activeControllers"].pop(i)
+            save()
 
 
 def removeDevice(data):
     for i, device in enumerate(config["connectedDevices"]):
         if device["mac"] == data["mac"]:
             config["connectedDevices"].pop(i)
+            save()
+
+
+def getDefault():
+    default = {
+        "activeControllers": [],
+        "deviceData": {},
+        "connectedDevices": []
+    }
+    return default
+
+
+def load():
+    global config
+    with open("config.json", "r") as f:
+        config = json.load(f)
+
+
+def save():
+    with open("config.json", "w") as f:
+        json.dump(config, f)
