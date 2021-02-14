49ebdd30a9b1bebbed23e7cac97933e7152ad5e6
Marc Leibold <leibold485@gmail.com>
2021-02-14 20:31:37 +0100

    added color and power state saving

--- espwebsite/helpers.py
+++ espwebsite/helpers.py
@@ -25,14 +25,20 @@ def getConnectedDevices():
         mac = device["mac"]
         if device["mac"] in deviceData:
             device["name"] = deviceData[mac]["name"]
+            device["color"] = deviceData[mac]["color"]
+            device["onState"] = deviceData[mac]["onState"]
             for key in device:
                 deviceData[mac][key] = device[key]
         else:
             deviceData[mac] = device
             deviceData[mac]["name"] = "_".join(mac.split(":"))
             deviceData[mac]["customColors"] = []
+            deviceData[mac]["color"] = {
+                "r": 255, "g": 255, "b": 255, "brightness": 150}
+            deviceData[mac]["onState"] = 0
             device["name"] = "_".join(device["mac"].split(":"))
     config.set("deviceData", deviceData)
+    config.set("connectedDevices", connectedDevices)
     return connectedDevices
 
 
@@ -45,6 +51,9 @@ def connect(data):
         if mac not in deviceData:
             deviceData[mac] = {"mac": mac}
             deviceData[mac]["customColors"] = []
+            deviceData[mac]["color"] = {
+                "r": 255, "g": 255, "b": 255, "brightness": 150}
+            deviceData[mac]["onState"] = 0
         deviceData[mac]["name"] = name
         config.set("deviceData", deviceData)
         config.add("connectedDevices", deviceData[mac])
@@ -90,4 +99,20 @@ def setRGB(data):
                     return False
                 else:
                     lastCall = time.time()
+
+                    if data["rgbb"] == {"r": 0, "g": 0, "b": 0, "brightness": 0}:
+                        deviceData[device]["onState"] = 0
+                    else:
+                        deviceData[device]["color"] = data["rgbb"]
+                        deviceData[device]["onState"] = 1
+                    config.set("deviceData", deviceData)
+
+                    connectedDevices = config.get("connectedDevices")
+                    for device in connectedDevices:
+                        if device["name"] == data["name"]:
+                            if data["rgbb"] == {"r": 0, "g": 0, "b": 0, "brightness": 0}:
+                                device["onState"] = 0
+                            else:
+                                device["onState"] = 1
+                    config.set("connectedDevices", connectedDevices)
                     return True
