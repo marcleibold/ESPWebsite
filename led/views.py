f649d9183a4d616f4b6c2f40d54b0eba05b17dc4
Marc Leibold <leibold485@gmail.com>
2021-09-05 19:01:13 +0200

    included first html and css

--- led/views.py
+++ led/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import Controller
 
 
 def led(request):
-    return render(request, 'base.html', {})
+    template = loader.get_template('base.html')
+    context = RequestContext(request, )
+    return HttpResponse(template.render({'controllers': Controller.objects.all()}))
