3a5b0bb983396c8919d0cbc5d797ab54a4a55687
Marc Leibold <leibold485@gmail.com>
2021-09-05 12:57:48 +0200

    changed SECRET_KEY

--- espwebsite/settings.py
+++ espwebsite/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(str(BASE_DIR) + "/.env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&gt=bkx=e!%z0rp)aexp@+h0fvoe1wc8g%oq2-mf80g&vcmrr2'
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
