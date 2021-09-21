000430e27d21f9195ab6919fe98f4ef48bc93865
Marc Leibold <leibold485@gmail.com>
2021-09-21 14:57:15 +0200

    first draft of config

---
+++ led/config.py
@@ -0,0 +1,24 @@
+class Config:
+    _conf = {
+        "MQTT": {
+            "broker": "192.168.178.73",
+            "port": 0,
+            "client_id": "",
+            "username": "",
+            "password": ""
+        }
+    }
+
+    @staticmethod
+    def get(key: str):
+        keys = key.split(".")
+        if len(keys) == 0 or (len(keys) == 1 and len(keys[0]) == 0):
+            raise AttributeError("You have to specify a key")
+        res = Config._conf
+        for i, k in enumerate(keys):
+            try:
+                res = res[k]
+            except KeyError:
+                prev = keys[i - 1] if i > 0 else "config"
+                raise KeyError(f"Key '{k}' not present in '{prev}'")
+        return res
