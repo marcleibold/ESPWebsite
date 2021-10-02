64e6962ff2044f19a4209c68cc6cfd1e73e642da
Marc Leibold <leibold485@gmail.com>
2021-10-02 16:22:28 +0200

    set /led as default

--- espwebsite/urls.py
+++ espwebsite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('led/', include('led.urls'))
+    path('led/', include('led.urls')),
+    path('', RedirectView.as_view(url='/led/', permanent=True))
 ]
